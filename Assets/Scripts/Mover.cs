a85b5e35321533b588345c192ab0055f6897d8c8
Martin Fučík <mar.fucik@gmail.com>
2020-06-21 19:13:32 +0200

    Chicken simple navigation

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target = null;
+
+    NavMeshAgent navMeshAgent = null;
+
+    private void Awake() 
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        navMeshAgent.destination = target.position;
+    }
+}
