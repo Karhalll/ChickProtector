bc9f96d9482bd5e5f01aa1786506920a95a47a34
Martin Fučík <mar.fucik@gmail.com>
2020-06-21 23:37:13 +0200

    Chicken Random Wandere

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -4,17 +4,50 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class Mover : MonoBehaviour
 {
-    [SerializeField] Transform target = null;
+    [SerializeField] float changeDirectionTime = 5f;
+    [SerializeField] float randomTargetRange = 20f;
 
     NavMeshAgent navMeshAgent = null;
+    float timer = 0f;
 
     private void Awake() 
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
+    private void Start() 
+    {
+        ChangeTarget();
+    }
+
     void Update()
     {
-        navMeshAgent.destination = target.position;
+        UpdateTimer();
+
+        if (timer > changeDirectionTime)
+        {
+            ChangeTarget();
+            timer = 0f;
+        }
+    }
+
+    private void ChangeTarget()
+    {
+        float randomX = Random.Range(transform.position.x - randomTargetRange, transform.position.x + randomTargetRange);
+        float randomZ = Random.Range(transform.position.z - randomTargetRange, transform.position.z + randomTargetRange);
+        
+        Vector3 newPosition = new Vector3(randomX, transform.position.y, randomZ);
+
+        NavMeshHit hit;
+        if (!NavMesh.SamplePosition(newPosition, out hit, 4f, NavMesh.AllAreas)) 
+        {
+            NavMesh.SamplePosition(newPosition, out hit, randomTargetRange + 4f, NavMesh.AllAreas);  
+        }
+        navMeshAgent.destination = hit.position;
+    }
+
+    private void UpdateTimer() 
+    {
+        timer += Time.deltaTime;
     }
 }
