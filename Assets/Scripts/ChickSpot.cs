d4b3d7131436af1ebb8dfe3c22f8061f6bfbd969
Martin Fučík <mar.fucik@gmail.com>
2020-07-24 16:32:10 +0200

    Start working on Henhouse basic functionality

---
+++ Assets/Scripts/ChickSpot.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ChickSpot : MonoBehaviour
+{
+    Chick chick;
+
+    public void ReleaseChick()
+    {
+
+    }
+
+    public void DockChick(Chick chick)
+    {
+        this.chick = chick;
+    }
+}
---
+++ Assets/Scripts/Henhouse.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Henhouse : MonoBehaviour
+{
+    [SerializeField] Transform chickSpots = null;
+
+    ChickSpot[] spots;
+    float releaseTimer;
+
+    void Start()
+    {
+        GetSpots();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void GetSpots()
+    {
+        spots = chickSpots.GetComponentsInChildren<ChickSpot>();
+    }
+}
